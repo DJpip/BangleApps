8570700482f6e2e0c1b107a28805395a975d2dbc
DJpip <125915092+DJpip@users.noreply.github.com>
2023-02-23 13:39:18 +0000

    Update app.js

--- apps/Class Companion/app.js
+++ apps/Class Companion/app.js
@@ -6,22 +6,22 @@
 
 
 const lessonTimes = {
-  0:  [8,20,30]
-  1:  [8,50,50]
-  2:  [9,40,50]
-  3:  [10,45,45]
-  4:  [11,30,50]
-  5:  [12,20,50]
-  6:  [13,10,50]
-  7:  [14,00,50]
-  8:  [14,50,50]
-  9:  [15,40,50]
+  0:  [8,20,30],
+  1:  [8,50,50],
+  2:  [9,40,50],
+  3:  [10,45,45],
+  4:  [11,30,50],
+  5:  [12,20,50],
+  6:  [13,10,50],
+  7:  [14,00,50],
+  8:  [14,50,50],
+  9:  [15,40,50],
 };
 
 //time variables. Declare and set on load.
   var Period = 0;
   
-  var t = new.Date();
+  var t = Date();
   var d = t.getDay();
   var h = t.getHours();
   var m = t.getMinutes();
@@ -32,7 +32,7 @@ const lessonTimes = {
 
 function getTime(){
   
-  t = new.Date(); 
+  t = Date(); 
   h = t.getHours(); Print(h);
   m = t.getMinutes(); Print(m);
   time = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2); print(time);
@@ -43,8 +43,9 @@ function getTime(){
   if(m !== lastM){
     lastM = m;
     lessonStart();
+  }
 }
-  
+
 function checkWeekend(){
   if(d > 5){
     isWeekend = true;
