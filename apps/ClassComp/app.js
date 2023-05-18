e0ce60eba791caf93ce1ae14ff4835103a524b48
DJpip <125915092+DJpip@users.noreply.github.com>
2023-05-18 20:57:47 +0100

    repairs to watch

--- apps/ClassComp/app.js
+++ apps/ClassComp/app.js
@@ -28,46 +28,37 @@ function LTInt(arr){
    timeInt[i][3] = arr[i][0]*60+arr[i][1]+arr[i][2]-15; //lesson 15 mins warning
    timeInt[i][4] = arr[i][0]*60+arr[i][1]+arr[i][2]-5; //lesson 5 mins warning
  }
- //print(timeInt);
 }
 
-const days = [
-  "Sun",
-  "Mon",
-  "Tue",
-  "Wed",
-  "Thu",
-  "Fri",
-  "Sat"
-];
-
-//Variables. Declare and set on load.
+//Variables. Declare and initialise. on load.
   const f1 = 1E3;  //of getTime updates
   var t = new Date();
   var d = t.getDay();
-  var day = days[d];
   var date = t.getDate(); //0..31 day value
   var month = t.getMonth()+1;
   var h = t.getHours();
   var m = t.getMinutes();
   var s = t.getSeconds();
   var lastM = t.getMinutes();
-  var isWeekend;
+  var isWeekend = true;
+  var isNight = true;
+  const morningStart = 6;
+  const dayEnd = 19;
   var time;
   var timeM;
-  let timeInt = [[],[]]; //see below
+  //TimeInt is array of [start time as int, end time as int, mid time as int, 3/4 //time as int, 5 mins remaining as int]
+  let timeInt = [[],[]]; 
 
   var waiting = false;
-  var loadRun = true;
-
+  
   var Period = 0;
   var lastPeriod = 0;
   var maxLessons = lessonTimes.length;
  
   var counter = 0;
-  var faceUpVar = false;
-  var tap1 = false;
-//TimeInt is array of [start time as int, end time as int, mid time as int, 3/4 //time as int, 5 mins remaining as int]
+ // var faceUpVar = false;
+ // var tap1 = false;
+
 
 //Load run here
 function load(){
@@ -78,22 +69,28 @@ g.clear();
     lessonUpdate(); //checks if lesson has changed, and if it needs to buzz
     Bangle.drawWidgets(); drawPeriod(); //draws the period top left
     getTime(); drawTime();
+    //check if it's night and we need to draw seconds or not
+    if(h >=  morningStart && h <= dayEnd){
+      isNight = false;
+    }
+    else{ isNight = true;}
 }
 
 
-function getTime(){ //every f1 seconds, gets the time and draws the seconds. runs the loadRun first time
+function getTime(){ //every f1 seconds, gets the time and draws the seconds.
   
   t = Date(); 
   h = t.getHours(); 
   m = t.getMinutes(); 
   s = t.getSeconds();
-  time = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2);// + ":" + ("0" + s).substr(-2);
+ // time = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2);
+  time = require("locale").time(t, 1);
   drawSeconds(138,85,25,30);
   
 }
 
 function drawSeconds(x,y,h,w){
-  if(!isWeekend){
+  if(!isWeekend && !isNight){
   g.clearRect(x,y,x+w,y+h);
   g.setColor(-1);
   g.setFont("Vector",h);
@@ -102,10 +99,41 @@ function drawSeconds(x,y,h,w){
   if(m !== lastM){ //check every new minute and update the lesson number and buzz if required
     lastM = m;
     lessonUpdate();
+    drawTime();
   }
-  if(s==0){drawTime();}
+  if(m == 0){
+    //check if it is night and we need to draw seconds 
+    if(h >=  morningStart && h <= dayEnd){
+      isNight = false;
+    }
+    else{ isNight = true;}
+  }
 }
 
+function drawTime(){
+  g.clearRect(0,31,240,85);
+  g.setColor(-1);
+  g.setFont("Vector",60);
+  g.drawString(time,10,31);
+  
+}
+
+function drawPeriod(){
+  Bangle.drawWidgets();
+  g.clearRect(25,0,100,30);
+  g.setFont("Vector",30);
+  g.setColor("#FFFF00");
+  if(isWeekend){g.drawString(day,25,0);}
+  else if(Period == 0){g.drawString(day,25,0);}
+  else{g.drawString(day + " P" + Period,25,0);}
+}
+
+function drawDate(x,y,h,w){
+  g.clearRect(x,y,x+w,y+h);
+  g.setColor("#00FFFF");
+  g.setFont("Vector",h);
+  g.drawString( date + "/" + month,x,y);
+}
 
 Bangle.on('midnight', function() { //updates the day on midnight
     checkDate();
@@ -114,7 +142,7 @@ Bangle.on('midnight', function() { //updates the day on midnight
 
 function checkDate(){
   d = t.getDay();
-  day = dow(t,1);
+  day = require("locale").dow(t, 1);
   date = t.getDate();
   month = t.getMonth() + 1;
   checkWeekend();
@@ -185,77 +213,10 @@ if(!waiting){ //don't buzz more than once in a minute
     },60001);
    }
 
-} // end of waiting if
-}
-
-function drawPeriod(){
-  Bangle.drawWidgets();
-  g.clearRect(25,0,100,30);
-  g.setFont("Vector",30);
-  g.setColor("#FFFF00");
-  if(isWeekend){g.drawString(day,25,0);}
-  else if(Period == 0){g.drawString(day,25,0);}
-  else{g.drawString(day + " P" + Period,25,0);}
+} 
 }
 
 
-
-function drawCounter(x,y,h,w){
-  Bangle.buzz(100,0.5);
-  g.clearRect(x,y,x+w,y+h);
-  if(counter>0){
-    g.setColor("#FF8F8F");//off red
-    g.setFont("Vector",h);
-    g.drawString(counter,x,y);
-  }
-}
-
-/*Bangle.on('faceUp',function(Up){
-  faceUpVar = Up;
-});
-
-Bangle.on('tap',function(data){
-  if(faceUpVar){
-    if(data.dir == "front"){
-      counter ++;
-      drawCounter(110,126,50,66);
-        if(data.double == true){
-          pen();
-         }
-    }
-    else if(data.dir == "left"){
-      checkHID();
-    }
-    else if(data.dir == "right"){
-      altTab();
-    }
-    else if(data.dir == "top"){
-      pressKey(26,0);
-    }
-    else if(data.dir == "bottom"){
-     counter = 0;
-      drawCounter(110,126,50,66);
-    }
-  }
-});
-*/
-Bangle.setOptions({gestureInactiveCount:4}); //default 4 samples before looking again. poll interval is 80ms after waking up.
-Bangle.setOptions({gestureMinLength:10}); //default 10 samples long
-
-function drawTime(){
-  g.clearRect(0,31,240,85);
-  g.setColor(-1);
-  g.setFont("Vector",60);
-  g.drawString(time,10,31);
-  
-}
-
-function drawDate(x,y,h,w){
-  g.clearRect(x,y,x+w,y+h);
-  g.setColor("#00FFFF");
-  g.setFont("Vector",h);
-  g.drawString( date + "/" + month,x,y);
-}
 const storage = require('Storage');
 const settings = storage.readJSON('setting.json',1) || { HID: false };
 //const kb = require("ble_hid_combo");
@@ -277,8 +238,7 @@ if (settings.HID === "com") {
     }
   });
 }
-  //drawApp();
-  //setInterval(update, 100); // 10 Hz
+
  else {
   E.showPrompt("Enable KB HID?",{title:"HID disabled"}).then(function(enable) {
     if (enable) {
@@ -295,33 +255,6 @@ if (settings.HID === "com") {
 
 let HIDenabled = true;
 
-//from tutorial
-/*
-int.tapKey(int.KEY.A);
-int.tapKey(int.KEY.A, int.MODIFY.SHIFT);
-int.clickButton(int.BUTTON.LEFT);
-// Scroll vertically and horizontally:
-  int.scroll(10, -20);
-  
- exports.MODIFY = {
-  CTRL        : 0x01,
-  SHIFT       : 0x02,
-  ALT         : 0x04,
-  GUI         : 0x08,
-  LEFT_CTRL   : 0x01,
-  LEFT_SHIFT  : 0x02,
-  LEFT_ALT    : 0x04,
-  LEFT_GUI    : 0x08,
-  RIGHT_CTRL  : 0x10,
-  RIGHT_SHIFT : 0x20,
-  RIGHT_ALT   : 0x40,
-  RIGHT_GUI   : 0x80
-};  //can be added CTRL + SHIFT
-*/
-
-/*function sendKey(keyVal){
-  int.tapKey(int.KEY.keyVal, 0);
-}*/
 
 function altTab(){
  //int.tapKey(int.KEY.T,int.MODIFY.ALT);
@@ -354,27 +287,13 @@ function clickMouse(b, callback) {
     });
   });
 }
-//NRF.setServices(undefined, { hid : kb.report });
 
-/*function moveMouse(x,y,b,wheel,hwheel,callback) {
-  if (!HIDenabled) return;
-  if (!b) b = 0;
-  if (!wheel) wheel = 0;
-  if (!hwheel) hwheel = 0;
-  NRF.sendHIDReport([1,b,x,y,wheel,hwheel,0,0], function() {
-    if (callback) callback();
-  });
-}
-function scroll(wheel,hwheel,callback) {
-  moveMouse(0,0,0,wheel,hwheel,callback);
-}
-*/
 
 load();
 Bangle.setUI("clock");
 Bangle.loadWidgets();
-Bangle.drawWidgets(); drawPeriod();
+Bangle.drawWidgets(); //drawPeriod();
 Bangle.on('lock', function(on) { drawPeriod();});
 
 setInterval(getTime,f1);
-getTime(); //timelyUpdates();
+
